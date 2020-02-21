bbb1e23a82a8fc21f6752dee994d267e7e36d5ce
Andrew D. Zonenberg <azonenberg@drawersteak.com>
2020-02-20 17:31:15 -0800

    CaptureChannel: added GetSampleEnd convenience function

--- scopehal/CaptureChannel.h
+++ scopehal/CaptureChannel.h
@@ -2,7 +2,7 @@
 *                                                                                                                      *
 * ANTIKERNEL v0.1                                                                                                      *
 *                                                                                                                      *
-* Copyright (c) 2012-2019 Andrew D. Zonenberg                                                                          *
+* Copyright (c) 2012-2020 Andrew D. Zonenberg                                                                          *
 * All rights reserved.                                                                                                 *
 *                                                                                                                      *
 * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the     *
@@ -79,6 +79,8 @@ public:
 	virtual int64_t GetEndTime() const =0;
 
 	virtual int64_t GetSampleStart(size_t i) const =0;
+	int64_t GetSampleEnd(size_t i) const
+	{ return GetSampleStart(i) + GetSampleLen(i); }
 	virtual int64_t GetSampleLen(size_t i) const =0;
 
 	virtual bool EqualityTest(size_t i, size_t j) const =0;
