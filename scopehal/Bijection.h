3d6ed37217d62b7edd08adb9e1082f919a75d344
Andrew D. Zonenberg <azonenberg@drawersteak.com>
2020-03-14 13:06:59 -0700

    Created Bijection and IDTable classes for more efficient serialization/loading

---
+++ scopehal/Bijection.h
@@ -0,0 +1,74 @@
+/***********************************************************************************************************************
+*                                                                                                                      *
+* ANTIKERNEL v0.1                                                                                                      *
+*                                                                                                                      *
+* Copyright (c) 2012-2020 Andrew D. Zonenberg                                                                          *
+* All rights reserved.                                                                                                 *
+*                                                                                                                      *
+* Redistribution and use in source and binary forms, with or without modification, are permitted provided that the     *
+* following conditions are met:                                                                                        *
+*                                                                                                                      *
+*    * Redistributions of source code must retain the above copyright notice, this list of conditions, and the         *
+*      following disclaimer.                                                                                           *
+*                                                                                                                      *
+*    * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the       *
+*      following disclaimer in the documentation and/or other materials provided with the distribution.                *
+*                                                                                                                      *
+*    * Neither the name of the author nor the names of any contributors may be used to endorse or promote products     *
+*      derived from this software without specific prior written permission.                                           *
+*                                                                                                                      *
+* THIS SOFTWARE IS PROVIDED BY THE AUTHORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED   *
+* TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL *
+* THE AUTHORS BE HELD LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES        *
+* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR       *
+* BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT *
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE       *
+* POSSIBILITY OF SUCH DAMAGE.                                                                                          *
+*                                                                                                                      *
+***********************************************************************************************************************/
+
+/**
+	@file
+	@author Andrew D. Zonenberg
+	@brief Declaration of Bijection class
+ */
+#ifndef Bijection_h
+#define Bijection_h
+
+/**
+	@brief A one-to-one mapping from objects of type T1 to type T2, which must be different types
+
+	For now insert-only, elements can be inserted or iterated but not removed.
+ */
+template<class T1, class T2>
+class Bijection
+{
+public:
+
+	typedef std::map<T1, T2> forwardType;
+	typedef std::map<T2, T1> reverseType;
+
+	typename forwardType::const_iterator begin()
+	{ return m_forwardMap.begin(); }
+
+	typename forwardType::const_iterator end()
+	{ return m_forwardMap.end(); }
+
+	void emplace(T1 a, T2 b)
+	{
+		m_forwardMap[a] = b;
+		m_reverseMap[b] = a;
+	}
+
+	const T1& operator[](T2 key)
+	{ return m_reverseMap[key]; }
+
+	const T2& operator[](T1 key)
+	{ return m_forwardMap[key]; }
+
+protected:
+	forwardType m_forwardMap;
+	reverseType m_reverseMap;
+};
+
+#endif
