35ded8dcb6c978524df63669a03a8fc7a2c0b0b0
Andrew D. Zonenberg <azonenberg@drawersteak.com>
2020-05-02 08:35:08 -0700

    Initial (very incomplete) BLONDEL oscilloscope driver

---
+++ scopehal/AntikernelLabsOscilloscope.cpp
@@ -0,0 +1,618 @@
+/***********************************************************************************************************************
+*                                                                                                                      *
+* ANTIKERNEL v0.1                                                                                                      *
+*                                                                                                                      *
+* Copyright (c) 2012-2020 Andrew D. Zonenberg                                                                          *
+* All rights reserved.                                                                                                 *
+*                                                                                                                      *
+* Redistribution and use in source and binary forms, with or without modification, are permitted provided that the     *
+* following conditions are met:                                                                                        *
+*                                                                                                                      *
+*    * Redistributions of source code must retain the above copyright notice, this list of conditions, and the         *
+*      following disclaimer.                                                                                           *
+*                                                                                                                      *
+*    * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the       *
+*      following disclaimer in the documentation and/or other materials provided with the distribution.                *
+*                                                                                                                      *
+*    * Neither the name of the author nor the names of any contributors may be used to endorse or promote products     *
+*      derived from this software without specific prior written permission.                                           *
+*                                                                                                                      *
+* THIS SOFTWARE IS PROVIDED BY THE AUTHORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED   *
+* TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL *
+* THE AUTHORS BE HELD LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES        *
+* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR       *
+* BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT *
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE       *
+* POSSIBILITY OF SUCH DAMAGE.                                                                                          *
+*                                                                                                                      *
+***********************************************************************************************************************/
+
+#include "scopehal.h"
+#include "AntikernelLabsOscilloscope.h"
+
+using namespace std;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Construction / destruction
+
+AntikernelLabsOscilloscope::AntikernelLabsOscilloscope(SCPITransport* transport)
+	: SCPIOscilloscope(transport)
+	/*, m_triggerChannelValid(false)
+	, m_triggerLevelValid(false)
+	, m_triggerArmed(false)
+	, m_triggerOneShot(false)*/
+{
+	//Last digit of the model number is the number of channels
+	//int model_number = atoi(m_model.c_str() + 3);	//FIXME: are all series IDs 3 chars e.g. "RTM"?
+	//int nchans = model_number % 10;
+
+	int nchans = 1;
+
+	for(int i=0; i<nchans; i++)
+	{
+		//Hardware name of the channel
+		string chname = string("C1");
+		chname[1] += i;
+
+		//Color the channels based on Antikernel Labs's color sequence
+		string color = "#ffffff";
+		switch(i)
+		{
+			case 0:
+				color = "#ffff80";
+				break;
+
+			case 1:
+				color = "#ff8080";
+				break;
+
+			case 2:
+				color = "#80ffff";
+				break;
+
+			case 3:
+				color = "#80ff80";
+				break;
+
+			//TODO: colors for the other 4 channels
+		}
+
+		//Create the channel
+		m_channels.push_back(
+			new OscilloscopeChannel(
+			this,
+			chname,
+			OscilloscopeChannel::CHANNEL_TYPE_ANALOG,
+			color,
+			1,
+			i,
+			true));
+
+		//Request all points when we download
+		//m_transport->SendCommand(chname + ":DATA:POIN MAX");
+	}
+	m_analogChannelCount = nchans;
+
+	/*
+	//Add the external trigger input
+	m_extTrigChannel = new OscilloscopeChannel(
+		this,
+		"EX",
+		OscilloscopeChannel::CHANNEL_TYPE_TRIGGER,
+		"",
+		1,
+		m_channels.size(),
+		true);
+	m_channels.push_back(m_extTrigChannel);
+	*/
+}
+
+AntikernelLabsOscilloscope::~AntikernelLabsOscilloscope()
+{
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Accessors
+
+string AntikernelLabsOscilloscope::GetDriverNameInternal()
+{
+	return "aklabs";
+}
+
+unsigned int AntikernelLabsOscilloscope::GetInstrumentTypes()
+{
+	return Instrument::INST_OSCILLOSCOPE;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Device interface functions
+
+void AntikernelLabsOscilloscope::FlushConfigCache()
+{
+	lock_guard<recursive_mutex> lock(m_cacheMutex);
+
+	//m_channelOffsets.clear();
+	m_channelVoltageRanges.clear();
+	/*
+	m_channelsEnabled.clear();
+	m_triggerChannelValid = false;
+	m_triggerLevelValid = false;
+	*/
+}
+
+bool AntikernelLabsOscilloscope::IsChannelEnabled(size_t i)
+{
+	/*
+	//ext trigger should never be displayed
+	if(i == m_extTrigChannel->GetIndex())
+		return false;
+
+	//TODO: handle digital channels, for now just claim they're off
+	if(i >= m_analogChannelCount)
+		return false;
+
+	lock_guard<recursive_mutex> lock(m_cacheMutex);
+
+	if(m_channelsEnabled.find(i) != m_channelsEnabled.end())
+		return m_channelsEnabled[i];
+
+	lock_guard<recursive_mutex> lock2(m_mutex);
+
+	m_transport->SendCommand(m_channels[i]->GetHwname() + ":STAT?");
+	string reply = m_transport->ReadReply();
+	if(reply == "OFF")
+	{
+		m_channelsEnabled[i] = false;
+		return false;
+	}
+	else
+	{
+		m_channelsEnabled[i] = true;
+		return true;
+	}
+	*/
+	return true;
+}
+
+void AntikernelLabsOscilloscope::EnableChannel(size_t i)
+{
+	lock_guard<recursive_mutex> lock(m_mutex);
+	//m_transport->SendCommand(m_channels[i]->GetHwname() + ":STAT ON");
+}
+
+void AntikernelLabsOscilloscope::DisableChannel(size_t i)
+{
+	lock_guard<recursive_mutex> lock(m_mutex);
+	//m_transport->SendCommand(m_channels[i]->GetHwname() + ":STAT OFF");
+}
+
+OscilloscopeChannel::CouplingType AntikernelLabsOscilloscope::GetChannelCoupling(size_t /*i*/)
+{
+	//All channels are 50 ohm all the time
+	return OscilloscopeChannel::COUPLE_DC_50;
+}
+
+void AntikernelLabsOscilloscope::SetChannelCoupling(size_t /*i*/, OscilloscopeChannel::CouplingType /*type*/)
+{
+	//No-op, all channels are 50 ohm all the time
+}
+
+double AntikernelLabsOscilloscope::GetChannelAttenuation(size_t /*i*/)
+{
+	/*
+	lock_guard<recursive_mutex> lock(m_mutex);
+
+	m_transport->SendCommand(m_channels[i]->GetHwname() + ":PROB?");
+
+	string reply = m_transport->ReadReply();
+	double atten;
+	sscanf(reply.c_str(), "%lf", &atten);
+	return atten;
+	*/
+
+	LogWarning("AntikernelLabsOscilloscope::GetChannelAttenuation unimplemented\n");
+	return 1;
+}
+
+void AntikernelLabsOscilloscope::SetChannelAttenuation(size_t /*i*/, double /*atten*/)
+{
+	//FIXME
+
+	LogWarning("AntikernelLabsOscilloscope::SetChannelAttenuation unimplemented\n");
+}
+
+int AntikernelLabsOscilloscope::GetChannelBandwidthLimit(size_t /*i*/)
+{
+	/*
+	lock_guard<recursive_mutex> lock(m_mutex);
+
+	m_transport->SendCommand(m_channels[i]->GetHwname() + ":BWL?");
+	string reply = m_transport->ReadReply();
+	if(reply == "20M")
+		return 20;
+	else
+		return 0;
+	*/
+
+	LogWarning("AntikernelLabsOscilloscope::GetChannelBandwidthLimit unimplemented\n");
+	return 0;
+}
+
+void AntikernelLabsOscilloscope::SetChannelBandwidthLimit(size_t /*i*/, unsigned int /*limit_mhz*/)
+{
+	LogWarning("AntikernelLabsOscilloscope::SetChannelBandwidthLimit unimplemented\n");
+}
+
+double AntikernelLabsOscilloscope::GetChannelVoltageRange(size_t i)
+{
+	{
+		lock_guard<recursive_mutex> lock(m_cacheMutex);
+		if(m_channelVoltageRanges.find(i) != m_channelVoltageRanges.end())
+			return m_channelVoltageRanges[i];
+	}
+
+	lock_guard<recursive_mutex> lock2(m_mutex);
+
+	m_transport->SendCommand(m_channels[i]->GetHwname() + ":GAIN?");
+	string reply = m_transport->ReadReply();
+
+	/*
+		Current firmware reports the gain of the VGA, not overall system gain (this will change eventually).
+		We have a -6 dB fixed attenuator before the VGA then 2 dB of fixed gain after it - so subtract 4 dB.
+	*/
+	int db;
+	sscanf(reply.c_str(), "%d", &db);
+	db -= 4;
+
+	float frac_gain = pow(10, db / 20.0f);
+	float vfs = 2.0 / frac_gain;
+
+	//LogDebug("Channel gain is %d dB (%.2f V/V, Vfs = %.3f)\n", db, frac_gain, vfs);
+
+	lock_guard<recursive_mutex> lock(m_cacheMutex);
+	m_channelVoltageRanges[i] = vfs;
+	return vfs;
+}
+
+void AntikernelLabsOscilloscope::SetChannelVoltageRange(size_t /*i*/, double /*range*/)
+{
+	//FIXME
+	LogWarning("AntikernelLabsOscilloscope::SetChannelVoltageRange unimplemented\n");
+}
+
+OscilloscopeChannel* AntikernelLabsOscilloscope::GetExternalTrigger()
+{
+	//FIXME
+	LogWarning("AntikernelLabsOscilloscope::GetExternalTrigger unimplemented\n");
+	return NULL;
+}
+
+double AntikernelLabsOscilloscope::GetChannelOffset(size_t i)
+{
+	/*
+	{
+		lock_guard<recursive_mutex> lock(m_cacheMutex);
+
+		if(m_channelOffsets.find(i) != m_channelOffsets.end())
+			return m_channelOffsets[i];
+	}
+
+	lock_guard<recursive_mutex> lock2(m_mutex);
+
+	m_transport->SendCommand(m_channels[i]->GetHwname() + ":OFFS?");
+
+	string reply = m_transport->ReadReply();
+	double offset;
+	sscanf(reply.c_str(), "%lf", &offset);
+	offset = -offset;
+	lock_guard<recursive_mutex> lock(m_cacheMutex);
+	m_channelOffsets[i] = offset;
+	return offset;
+	*/
+	return 0;
+}
+
+void AntikernelLabsOscilloscope::SetChannelOffset(size_t /*i*/, double /*offset*/)
+{
+	//FIXME
+	LogWarning("AntikernelLabsOscilloscope::SetChannelOffset unimplemented\n");
+}
+
+void AntikernelLabsOscilloscope::ResetTriggerConditions()
+{
+	//FIXME
+	LogWarning("AntikernelLabsOscilloscope::ResetTriggerConditions unimplemented\n");
+}
+
+Oscilloscope::TriggerMode AntikernelLabsOscilloscope::PollTrigger()
+{
+	/*
+	lock_guard<recursive_mutex> lock(m_mutex);
+
+	m_transport->SendCommand("ACQ:STAT?");
+	string stat = m_transport->ReadReply();
+
+	if(stat == "RUN")
+		return TRIGGER_MODE_RUN;
+	else if( (stat == "STOP") || (stat == "BRE") )
+		return TRIGGER_MODE_STOP;
+	else if(stat == "COMP")
+	{
+		m_triggerArmed = false;
+		return TRIGGER_MODE_TRIGGERED;
+	}
+	*/
+	return TRIGGER_MODE_STOP;
+}
+
+bool AntikernelLabsOscilloscope::AcquireData(bool toQueue)
+{
+	/*
+	//LogDebug("Acquiring data\n");
+
+	lock_guard<recursive_mutex> lock(m_mutex);
+	LogIndenter li;
+
+	double xstart;
+	double xstop;
+	size_t length;
+	int ignored;
+	map<int, vector<AnalogCapture*> > pending_waveforms;
+	for(size_t i=0; i<m_analogChannelCount; i++)
+	{
+		if(!IsChannelEnabled(i))
+		{
+			if(!toQueue)
+				m_channels[i]->SetData(NULL);
+			continue;
+		}
+
+		//This is basically the same function as a LeCroy WAVEDESC, but much less detailed
+		m_transport->SendCommand(m_channels[i]->GetHwname() + ":DATA:HEAD?");
+		string reply = m_transport->ReadReply();
+		sscanf(reply.c_str(), "%lf,%lf,%zu,%d", &xstart, &xstop, &length, &ignored);
+
+		//Figure out the sample rate
+		double capture_len_sec = xstop - xstart;
+		double sec_per_sample = capture_len_sec / length;
+		int64_t ps_per_sample = round(sec_per_sample * 1e12f);
+		//LogDebug("%ld ps/sample\n", ps_per_sample);
+
+		float* temp_buf = new float[length];
+
+		//Set up the capture we're going to store our data into (no high res timer on R&S scopes)
+		AnalogCapture* cap = new AnalogCapture;
+		cap->m_timescale = ps_per_sample;
+		cap->m_triggerPhase = 0;
+		cap->m_startTimestamp = time(NULL);
+		double t = GetTime();
+		cap->m_startPicoseconds = (t - floor(t)) * 1e12f;
+
+		//Ask for the data
+		m_transport->SendCommand(m_channels[i]->GetHwname() + ":DATA?");
+
+		//Read and discard the length header
+		char tmp[16] = {0};
+		m_transport->ReadRawData(2, (unsigned char*)tmp);
+		int num_digits = atoi(tmp+1);
+		m_transport->ReadRawData(num_digits, (unsigned char*)tmp);
+		int actual_len = atoi(tmp);
+
+		//Read the actual data
+		m_transport->ReadRawData(length*sizeof(float), (unsigned char*)temp_buf);
+
+		//Format the capture
+		for(size_t i=0; i<length; i++)
+			cap->m_samples.push_back(AnalogSample(i, 1, temp_buf[i]));
+
+		//Done, update the data
+		if(!toQueue)
+			m_channels[i]->SetData(cap);
+		else
+			pending_waveforms[i].push_back(cap);
+
+		//Clean up
+		delete[] temp_buf;
+	}
+
+	//Now that we have all of the pending waveforms, save them in sets across all channels
+	m_pendingWaveformsMutex.lock();
+	size_t num_pending = 0;
+	if(toQueue)				//if saving to queue, the 0'th segment counts too
+		num_pending ++;
+	for(size_t i=0; i<num_pending; i++)
+	{
+		SequenceSet s;
+		for(size_t j=0; j<m_analogChannelCount; j++)
+		{
+			if(IsChannelEnabled(j))
+				s[m_channels[j]] = pending_waveforms[j][i];
+		}
+		m_pendingWaveforms.push_back(s);
+	}
+	m_pendingWaveformsMutex.unlock();
+
+	//TODO: support digital channels
+
+	//Re-arm the trigger if not in one-shot mode
+	if(!m_triggerOneShot)
+	{
+		m_transport->SendCommand("SING");
+		m_triggerArmed = true;
+	}
+
+	//LogDebug("Acquisition done\n");
+	*/
+	return true;
+}
+
+void AntikernelLabsOscilloscope::Start()
+{
+	/*
+	lock_guard<recursive_mutex> lock(m_mutex);
+	m_transport->SendCommand("SING");
+	m_triggerArmed = true;
+	m_triggerOneShot = false;
+	*/
+}
+
+void AntikernelLabsOscilloscope::StartSingleTrigger()
+{
+	/*
+	lock_guard<recursive_mutex> lock(m_mutex);
+	m_transport->SendCommand("SING");
+	m_triggerArmed = true;
+	m_triggerOneShot = true;
+	*/
+}
+
+void AntikernelLabsOscilloscope::Stop()
+{
+	/*
+	lock_guard<recursive_mutex> lock(m_mutex);
+	m_transport->SendCommand("STOP");
+	m_triggerArmed = false;
+	m_triggerOneShot = true;
+	*/
+}
+
+bool AntikernelLabsOscilloscope::IsTriggerArmed()
+{
+	//return m_triggerArmed;
+	return true;
+}
+
+size_t AntikernelLabsOscilloscope::GetTriggerChannelIndex()
+{
+	/*
+	//Check cache
+	//No locking, worst case we return a result a few seconds old
+	if(m_triggerChannelValid)
+		return m_triggerChannel;
+
+	lock_guard<recursive_mutex> lock(m_mutex);
+
+	//Look it up
+	m_transport->SendCommand("TRIG:A:SOUR?");
+	string ret = m_transport->ReadReply();
+
+	//This is a bit annoying because the hwname's used here are DIFFERENT than everywhere else!
+	if(ret.find("CH") == 0)
+	{
+		m_triggerChannelValid = true;
+		m_triggerChannel = atoi(ret.c_str()+2) - 1;
+		return m_triggerChannel;
+	}
+	else if(ret == "EXT")
+	{
+		m_triggerChannelValid = true;
+		m_triggerChannel = m_extTrigChannel->GetIndex();
+		return m_triggerChannel;
+	}
+	else
+	{
+		m_triggerChannelValid = false;
+		LogWarning("Unknown trigger source %s\n", ret.c_str());
+		return 0;
+	}
+	*/
+	return 0;
+}
+
+void AntikernelLabsOscilloscope::SetTriggerChannelIndex(size_t /*i*/)
+{
+	//FIXME
+	LogWarning("AntikernelLabsOscilloscope::SetTriggerChannelIndex unimplemented\n");
+}
+
+float AntikernelLabsOscilloscope::GetTriggerVoltage()
+{
+	/*
+	//Check cache.
+	//No locking, worst case we return a just-invalidated (but still fresh-ish) result.
+	if(m_triggerLevelValid)
+		return m_triggerLevel;
+
+	lock_guard<recursive_mutex> lock(m_mutex);
+
+	m_transport->SendCommand("TRIG:A:LEV?");
+	string ret = m_transport->ReadReply();
+
+	double level;
+	sscanf(ret.c_str(), "%lf", &level);
+	m_triggerLevel = level;
+	m_triggerLevelValid = true;
+	return level;
+	*/
+	return 0;
+}
+
+void AntikernelLabsOscilloscope::SetTriggerVoltage(float /*v*/)
+{
+	//FIXME
+	LogWarning("AntikernelLabsOscilloscope::SetTriggerVoltage unimplemented\n");
+}
+
+Oscilloscope::TriggerType AntikernelLabsOscilloscope::GetTriggerType()
+{
+	//FIXME
+	LogWarning("AntikernelLabsOscilloscope::GetTriggerType unimplemented\n");
+	return Oscilloscope::TRIGGER_TYPE_RISING;
+}
+
+void AntikernelLabsOscilloscope::SetTriggerType(Oscilloscope::TriggerType /*type*/)
+{
+	//FIXME
+	LogWarning("AntikernelLabsOscilloscope::SetTriggerType unimplemented\n");
+}
+
+void AntikernelLabsOscilloscope::SetTriggerForChannel(OscilloscopeChannel* /*channel*/, vector<TriggerType> /*triggerbits*/)
+{
+	//unimplemented, no LA support
+	LogWarning("AntikernelLabsOscilloscope::SetTriggerForChannel unimplemented\n");
+}
+
+vector<uint64_t> AntikernelLabsOscilloscope::GetSampleRatesNonInterleaved()
+{
+	LogWarning("AntikernelLabsOscilloscope::GetSampleRatesNonInterleaved unimplemented\n");
+
+	//FIXME
+	vector<uint64_t> ret;
+	return ret;
+}
+
+vector<uint64_t> AntikernelLabsOscilloscope::GetSampleRatesInterleaved()
+{
+	LogWarning("AntikernelLabsOscilloscope::GetSampleRatesInterleaved unimplemented\n");
+
+	//FIXME
+	vector<uint64_t> ret;
+	return ret;
+}
+
+set<Oscilloscope::InterleaveConflict> AntikernelLabsOscilloscope::GetInterleaveConflicts()
+{
+	LogWarning("AntikernelLabsOscilloscope::GetInterleaveConflicts unimplemented\n");
+
+	//FIXME
+	set<Oscilloscope::InterleaveConflict> ret;
+	return ret;
+}
+
+vector<uint64_t> AntikernelLabsOscilloscope::GetSampleDepthsNonInterleaved()
+{
+	LogWarning("AntikernelLabsOscilloscope::GetSampleDepthsNonInterleaved unimplemented\n");
+
+	//FIXME
+	vector<uint64_t> ret;
+	return ret;
+}
+
+vector<uint64_t> AntikernelLabsOscilloscope::GetSampleDepthsInterleaved()
+{
+	LogWarning("AntikernelLabsOscilloscope::GetSampleDepthsInterleaved unimplemented\n");
+
+	//FIXME
+	vector<uint64_t> ret;
+	return ret;
+}
--- scopehal/AntikernelLabsOscilloscope.h
+++ scopehal/AntikernelLabsOscilloscope.h
@@ -27,132 +27,87 @@
 *                                                                                                                      *
 ***********************************************************************************************************************/
 
-/**
-	@file
-	@author Andrew D. Zonenberg
-	@brief Implementation of global functions
- */
-#include "scopehal.h"
-#include <gtkmm/drawingarea.h>
-#include "AgilentOscilloscope.h"
-#include "AntikernelLabsOscilloscope.h"
-#include "AntikernelLogicAnalyzer.h"
-#include "LeCroyOscilloscope.h"
-#include "RigolOscilloscope.h"
-#include "RohdeSchwarzOscilloscope.h"
-#include "SiglentSCPIOscilloscope.h"
-#include <libgen.h>
-#include <dlfcn.h>
+#ifndef AntikernelLabsOscilloscope_h
+#define AntikernelLabsOscilloscope_h
 
-using namespace std;
-
-/**
-	@brief Static initialization for SCPI transports
- */
-void TransportStaticInit()
+class AntikernelLabsOscilloscope : public SCPIOscilloscope
 {
-	AddTransportClass(SCPISocketTransport);
-	AddTransportClass(VICPSocketTransport);
-}
+public:
+	AntikernelLabsOscilloscope(SCPITransport* transport);
+	virtual ~AntikernelLabsOscilloscope();
 
-/**
-	@brief Static initialization for oscilloscopes
- */
-void DriverStaticInit()
-{
-	AddDriverClass(AgilentOscilloscope);
-	AddDriverClass(AntikernelLabsOscilloscope);
-	AddDriverClass(AntikernelLogicAnalyzer);
-	AddDriverClass(LeCroyOscilloscope);
-	AddDriverClass(RigolOscilloscope);
-	AddDriverClass(RohdeSchwarzOscilloscope);
-	AddDriverClass(SiglentSCPIOscilloscope);
-}
+public:
+	//Device information
+	virtual unsigned int GetInstrumentTypes();
 
-string GetDefaultChannelColor(int i)
-{
-	const int NUM_COLORS = 12;
-	static const char* colorTable[NUM_COLORS]=
-	{
-		"#a6cee3",
-		"#1f78b4",
-		"#b2df8a",
-		"#33a02c",
-		"#fb9a99",
-		"#e31a1c",
-		"#fdbf6f",
-		"#ff7f00",
-		"#cab2d6",
-		"#6a3d9a",
-		"#ffff99",
-		"#b15928"
-	};
+	virtual void FlushConfigCache();
 
-	return colorTable[i % NUM_COLORS];
-}
+	//Channel configuration
+	virtual bool IsChannelEnabled(size_t i);
+	virtual void EnableChannel(size_t i);
+	virtual void DisableChannel(size_t i);
+	virtual OscilloscopeChannel::CouplingType GetChannelCoupling(size_t i);
+	virtual void SetChannelCoupling(size_t i, OscilloscopeChannel::CouplingType type);
+	virtual double GetChannelAttenuation(size_t i);
+	virtual void SetChannelAttenuation(size_t i, double atten);
+	virtual int GetChannelBandwidthLimit(size_t i);
+	virtual void SetChannelBandwidthLimit(size_t i, unsigned int limit_mhz);
+	virtual double GetChannelVoltageRange(size_t i);
+	virtual void SetChannelVoltageRange(size_t i, double range);
+	virtual OscilloscopeChannel* GetExternalTrigger();
+	virtual double GetChannelOffset(size_t i);
+	virtual void SetChannelOffset(size_t i, double offset);
 
-/**
-	@brief Converts a vector bus signal into a scalar (up to 64 bits wide)
- */
-uint64_t ConvertVectorSignalToScalar(vector<bool> bits)
-{
-	uint64_t rval = 0;
-	for(auto b : bits)
-		rval = (rval << 1) | b;
-	return rval;
-}
+	//Triggering
+	virtual void ResetTriggerConditions();
+	virtual Oscilloscope::TriggerMode PollTrigger();
+	virtual bool AcquireData(bool toQueue = false);
+	virtual void Start();
+	virtual void StartSingleTrigger();
+	virtual void Stop();
+	virtual bool IsTriggerArmed();
+	virtual size_t GetTriggerChannelIndex();
+	virtual void SetTriggerChannelIndex(size_t i);
+	virtual float GetTriggerVoltage();
+	virtual void SetTriggerVoltage(float v);
+	virtual Oscilloscope::TriggerType GetTriggerType();
+	virtual void SetTriggerType(Oscilloscope::TriggerType type);
+	virtual void SetTriggerForChannel(OscilloscopeChannel* channel, std::vector<TriggerType> triggerbits);
 
-/**
-	@brief Initialize all plugins
- */
-void InitializePlugins()
-{
-	char tmp[1024];
-	vector<string> search_dirs;
-	search_dirs.push_back("/usr/lib/scopehal/plugins/");
-	search_dirs.push_back("/usr/local/lib/scopehal/plugins/");
+	virtual std::vector<uint64_t> GetSampleRatesNonInterleaved();
+	virtual std::vector<uint64_t> GetSampleRatesInterleaved();
+	virtual std::set<InterleaveConflict> GetInterleaveConflicts();
+	virtual std::vector<uint64_t> GetSampleDepthsNonInterleaved();
+	virtual std::vector<uint64_t> GetSampleDepthsInterleaved();
 
-	//current binary dir
-	char selfPath[1024] = {0};
-	ssize_t readlinkReturn = readlink("/proc/self/exe", selfPath, (sizeof(selfPath) - 1) );
-	if ( readlinkReturn > 0)
-		search_dirs.push_back(dirname(selfPath));
+protected:
 
-	//Home directory
-	snprintf(tmp, sizeof(tmp), "%s/.scopehal/plugins", getenv("HOME"));
-	search_dirs.push_back(tmp);
+	//OscilloscopeChannel* m_extTrigChannel;
 
-	for(auto dir : search_dirs)
-	{
-		DIR* hdir = opendir(dir.c_str());
-		if(!hdir)
-			continue;
+	//Mutexing for thread safety
+	std::recursive_mutex m_mutex;
+	std::recursive_mutex m_cacheMutex;
 
-		dirent* pent;
-		while((pent = readdir(hdir)))
-		{
-			//Don't load hidden files or parent directory entries
-			if(pent->d_name[0] == '.')
-				continue;
+	//hardware analog channel count, independent of LA option etc
+	unsigned int m_analogChannelCount;
 
-			//Try loading it and see if it works.
-			//(for now, never unload the plugins)
-			string fname = dir + "/" + pent->d_name;
-			void* hlib = dlopen(fname.c_str(), RTLD_NOW);
-			if(hlib == NULL)
-				continue;
+	//config cache
+	//std::map<size_t, double> m_channelOffsets;
+	std::map<size_t, double> m_channelVoltageRanges;
+	/*
+	std::map<int, bool> m_channelsEnabled;
+	bool m_triggerChannelValid;
+	size_t m_triggerChannel;
+	bool m_triggerLevelValid;
+	float m_triggerLevel;
 
-			//If loaded, look for PluginInit()
-			typedef void (*PluginInit)();
-			PluginInit init = (PluginInit)dlsym(hlib, "PluginInit");
-			if(!init)
-				continue;
+	bool m_triggerArmed;
+	bool m_triggerOneShot;
+	*/
 
-			//If found, it's a valid plugin
-			LogDebug("Loading plugin %s\n", fname.c_str());
-			init();
-		}
+public:
+	static std::string GetDriverNameInternal();
+	OSCILLOSCOPE_INITPROC(AntikernelLabsOscilloscope)
+};
 
-		closedir(hdir);
-	}
-}
+#endif
