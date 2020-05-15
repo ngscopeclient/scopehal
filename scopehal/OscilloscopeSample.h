9fd5f0a1b6d9e51f28f705a19dbad5e3f900bcd2
Andrew D. Zonenberg <azonenberg@drawersteak.com>
2020-05-14 20:43:09 -0700

    OscilloscopeSample: added empty default constructor for STL to use

--- scopehal/OscilloscopeSample.h
+++ scopehal/OscilloscopeSample.h
@@ -2,7 +2,7 @@
 *                                                                                                                      *
 * ANTIKERNEL v0.1                                                                                                      *
 *                                                                                                                      *
-* Copyright (c) 2012-2017 Andrew D. Zonenberg                                                                          *
+* Copyright (c) 2012-2020 Andrew D. Zonenberg                                                                          *
 * All rights reserved.                                                                                                 *
 *                                                                                                                      *
 * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the     *
@@ -44,7 +44,10 @@
 class OscilloscopeSampleBase
 {
 public:
-	OscilloscopeSampleBase(int64_t off = 0, int64_t dur = 0)
+	OscilloscopeSampleBase()
+	{}
+
+	OscilloscopeSampleBase(int64_t off, int64_t dur)
 	: m_offset(off)
 	, m_duration(dur)
 	{}
