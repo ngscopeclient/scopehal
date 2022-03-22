069135021e087bfc8c7ea32ff7d4cc959667279b
Andrew D. Zonenberg <azonenberg@drawersteak.com>
2022-03-21 20:06:04 -0700

    Fixed typos in copyright line

--- scopehal/RemoteBridgeOscilloscope.h
+++ scopehal/RemoteBridgeOscilloscope.h
@@ -2,7 +2,7 @@
 *                                                                                                                      *
 * libscopehal v0.1                                                                                                     *
 *                                                                                                                      *
-* Copyright (c) 2022- Andrew D. Zonenberg and contributors                                                         *
+* Copyright (c) 2012-2022 Andrew D. Zonenberg and contributors                                                         *
 * All rights reserved.                                                                                                 *
 *                                                                                                                      *
 * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the     *
@@ -52,7 +52,7 @@ public:
 
 	virtual float GetChannelVoltageRange(size_t i, size_t stream);
 	virtual void SetChannelVoltageRange(size_t i, size_t stream, float range);
-	
+
 	virtual float GetChannelOffset(size_t i, size_t stream);
 	virtual void SetChannelOffset(size_t i, size_t stream, float offset);
 
