36c5a75e48f9e6ad80f03443fc34be3966987dc1
Andrew D. Zonenberg <azonenberg@drawersteak.com>
2022-05-29 10:25:57 -0700

    Filter: dense pack optimizations to SampleOnAnyEdges

--- scopehal/Filter.cpp
+++ scopehal/Filter.cpp
@@ -497,25 +497,57 @@ void Filter::SampleOnAnyEdges(DigitalWaveform* data, DigitalWaveform* clock, Dig
 	//TODO: split up into blocks and multithread?
 	//TODO: AVX vcompress
 
-	size_t ndata = 0;
 	size_t len = clock->m_offsets.size();
 	size_t dlen = data->m_samples.size();
-	for(size_t i=1; i<len; i++)
+
+	//Optimizations for dense packed data
+	//Clock is typically generated from a CDR PLL and is thus rarely, if ever, dense packed.
+	//Not worth special casing there, but data coming from a scope etc is typically dense packed.
+	if(data->m_densePacked)
 	{
-		//Throw away clock samples until we find an edge
-		if(clock->m_samples[i] == clock->m_samples[i-1])
-			continue;
+		int64_t ndata = 0;
+		for(size_t i=1; i<len; i++)
+		{
+			//Throw away clock samples until we find an edge
+			if(clock->m_samples[i] == clock->m_samples[i-1])
+				continue;
 
-		//Throw away data samples until the data is synced with us
-		int64_t clkstart = clock->m_offsets[i] * clock->m_timescale + clock->m_triggerPhase;
-		while( (ndata+1 < dlen) && ((data->m_offsets[ndata+1] * data->m_timescale + data->m_triggerPhase) < clkstart) )
-			ndata ++;
-		if(ndata >= dlen)
-			break;
+			//Throw away data samples until the data is synced with us
+			//This is a bit more math but is often faster than a division
+			int64_t clkstart = clock->m_offsets[i] * clock->m_timescale + clock->m_triggerPhase;
+			int64_t clkstart2 = clkstart - data->m_triggerPhase;
+			while(((ndata+1) * data->m_timescale) < clkstart2)
+				ndata ++;
+			if((size_t)ndata >= dlen)
+				break;
 
-		//Add the new sample
-		samples.m_offsets.push_back(clkstart);
-		samples.m_samples.push_back(data->m_samples[ndata]);
+			//Add the new sample
+			samples.m_offsets.push_back(clkstart);
+			samples.m_samples.push_back(data->m_samples[ndata]);
+		}
+	}
+
+	//Generic implementation
+	else
+	{
+		size_t ndata = 0;
+		for(size_t i=1; i<len; i++)
+		{
+			//Throw away clock samples until we find an edge
+			if(clock->m_samples[i] == clock->m_samples[i-1])
+				continue;
+
+			//Throw away data samples until the data is synced with us
+			int64_t clkstart = clock->m_offsets[i] * clock->m_timescale + clock->m_triggerPhase;
+			while( (ndata+1 < dlen) && ((data->m_offsets[ndata+1] * data->m_timescale + data->m_triggerPhase) < clkstart) )
+				ndata ++;
+			if(ndata >= dlen)
+				break;
+
+			//Add the new sample
+			samples.m_offsets.push_back(clkstart);
+			samples.m_samples.push_back(data->m_samples[ndata]);
+		}
 	}
 
 	//Compute sample durations
