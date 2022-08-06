9bde02598923d4a01921aaa879abe382838f880a
Mike Walters <mike@flomp.net>
2022-08-06 17:10:05 +0100

    Add linux-gpib transport

--- scopehal/scopehal.h
+++ scopehal/scopehal.h
@@ -75,6 +75,7 @@
 #include "SCPITransport.h"
 #include "SCPISocketTransport.h"
 #include "SCPITwinLanTransport.h"
+#include "SCPILinuxGPIBTransport.h"
 #include "SCPILxiTransport.h"
 #include "SCPINullTransport.h"
 #include "SCPITMCTransport.h"
