0b80b3105d732cc304243ef9649b823e130550bb
Mitchell Johnson <ehntoo@ehntoo.org>
2022-09-17 11:28:43 -0400

    Initial implementation for GWInstek GPD-(X)303S

---
+++ scopehal/GWInstekGPDX303SPowerSupply.cpp
@@ -0,0 +1,213 @@
+#include "scopehal.h"
+#include "GWInstekGPDX303SPowerSupply.h"
+
+using namespace std;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Construction / destruction
+
+GWInstekGPDX303SPowerSupply::GWInstekGPDX303SPowerSupply(SCPITransport* transport)
+	: SCPIDevice(transport)
+	, SCPIInstrument(transport)
+{
+    auto modelNumber = atoi(m_model.c_str() + strlen("GPD-"));
+	// The GPD-3303S/D models have three channels, but only two are programmable and visible via SCPI
+    if (modelNumber == 3303) {
+        m_channelCount = 2;
+    } else {
+        m_channelCount = modelNumber/1000;
+    }
+}
+
+GWInstekGPDX303SPowerSupply::~GWInstekGPDX303SPowerSupply()
+{
+
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Device info
+
+string GWInstekGPDX303SPowerSupply::GetDriverNameInternal()
+{
+	return "gwinstek_gpdx303s";
+}
+
+
+string GWInstekGPDX303SPowerSupply::GetName()
+{
+	return m_model;
+}
+
+string GWInstekGPDX303SPowerSupply::GetVendor()
+{
+	return m_vendor;
+}
+
+string GWInstekGPDX303SPowerSupply::GetSerial()
+{
+	return m_serial;
+}
+
+unsigned int GWInstekGPDX303SPowerSupply::GetInstrumentTypes()
+{
+	return INST_PSU;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Actual hardware interfacing
+
+bool GWInstekGPDX303SPowerSupply::IsPowerConstantCurrent(int chan)
+{
+	int reg = GetStatusRegister();
+    if (chan >= 2) {
+        // TODO - examine a real-world output of the `STATUS?` command on a GPD-4303S, STATUS? is only documented for two channels in the user manual.
+        LogError("Error: CC/CV status encoding unknown for 3/4 channel scopes.\n");
+    }
+	return !(reg & (1 << chan));
+}
+
+uint8_t GWInstekGPDX303SPowerSupply::GetStatusRegister()
+{
+	lock_guard<recursive_mutex> lock(m_transport->GetMutex());
+
+	//Get status register
+	auto ret = m_transport->SendCommandQueuedWithReply("STATUS?");
+    // 8 bits in the following format:
+    // Bit    Item     Description
+    // 0      CH1      0=CC mode, 1=CV mode
+    // 1      CH2      0=CC mode, 1=CV mode
+    // 2, 3   Tracking 01=Independent, 11=Tracking series, 10=Tracking parallel
+    // 4      Beep     0=Off, 1=On
+    // 5      Output   0=Off, 1=On
+    // 6, 7   Baud     00=115200bps, 01=57600bps, 10=9600bps
+	return atoi(ret.c_str());
+}
+
+int GWInstekGPDX303SPowerSupply::GetPowerChannelCount()
+{
+	return m_channelCount;
+}
+
+string GWInstekGPDX303SPowerSupply::GetPowerChannelName(int chan)
+{
+	char tmp[] = "CH1";
+	tmp[2] += chan;
+	return string(tmp);
+}
+
+double GWInstekGPDX303SPowerSupply::GetPowerVoltageActual(int chan)
+{
+	lock_guard<recursive_mutex> lock(m_transport->GetMutex());
+
+    char tmpCmd[] = "VOUT1?";
+    tmpCmd[4] += chan;
+	auto ret = m_transport->SendCommandQueuedWithReply(string(tmpCmd));
+	return atof(ret.c_str());
+}
+
+double GWInstekGPDX303SPowerSupply::GetPowerVoltageNominal(int chan)
+{
+	lock_guard<recursive_mutex> lock(m_transport->GetMutex());
+
+    char tmpCmd[] = "VSET1?";
+    tmpCmd[4] += chan;
+	auto ret = m_transport->SendCommandQueuedWithReply(string(tmpCmd));
+	return atof(ret.c_str());
+}
+
+double GWInstekGPDX303SPowerSupply::GetPowerCurrentActual(int chan)
+{
+	lock_guard<recursive_mutex> lock(m_transport->GetMutex());
+
+    char tmpCmd[] = "IOUT1?";
+    tmpCmd[4] += chan;
+	auto ret = m_transport->SendCommandQueuedWithReply(string(tmpCmd));
+	return atof(ret.c_str());
+}
+
+double GWInstekGPDX303SPowerSupply::GetPowerCurrentNominal(int chan)
+{
+	lock_guard<recursive_mutex> lock(m_transport->GetMutex());
+
+    char tmpCmd[] = "ISET1?";
+    tmpCmd[4] += chan;
+	auto ret = m_transport->SendCommandQueuedWithReply(string(tmpCmd));
+	return atof(ret.c_str());
+}
+
+bool GWInstekGPDX303SPowerSupply::GetPowerChannelActive(int chan)
+{
+    (void) chan;
+    return true;
+}
+
+bool GWInstekGPDX303SPowerSupply::IsSoftStartEnabled(int chan)
+{
+    (void) chan;
+    return false;
+}
+
+void GWInstekGPDX303SPowerSupply::SetSoftStartEnabled(int chan, bool enable)
+{
+    (void) chan;
+    (void) enable;
+}
+
+void GWInstekGPDX303SPowerSupply::SetPowerOvercurrentShutdownEnabled(int chan, bool enable)
+{
+    (void) chan;
+    (void) enable;
+}
+
+bool GWInstekGPDX303SPowerSupply::GetPowerOvercurrentShutdownEnabled(int chan)
+{
+    (void) chan;
+    return false;
+}
+
+bool GWInstekGPDX303SPowerSupply::GetPowerOvercurrentShutdownTripped(int chan)
+{
+    (void) chan;
+    return false;
+}
+
+void GWInstekGPDX303SPowerSupply::SetPowerVoltage(int chan, double volts)
+{
+	lock_guard<recursive_mutex> lock(m_transport->GetMutex());
+
+	char cmd[128];
+	snprintf(cmd, sizeof(cmd), "VSET%u:%.3f", chan, volts);
+	m_transport->SendCommandQueued(cmd);
+}
+
+void GWInstekGPDX303SPowerSupply::SetPowerCurrent(int chan, double amps)
+{
+	lock_guard<recursive_mutex> lock(m_transport->GetMutex());
+
+	char cmd[128];
+	snprintf(cmd, sizeof(cmd), "ISET%u:%.3f", chan, amps);
+	m_transport->SendCommandQueued(cmd);
+}
+
+void GWInstekGPDX303SPowerSupply::SetPowerChannelActive(int chan, bool on)
+{
+    (void) chan;
+    (void) on;
+}
+
+bool GWInstekGPDX303SPowerSupply::GetMasterPowerEnable()
+{
+	lock_guard<recursive_mutex> lock(m_transport->GetMutex());
+
+	int reg = GetStatusRegister();
+	return (reg & (1 << 5));
+}
+
+void GWInstekGPDX303SPowerSupply::SetMasterPowerEnable(bool enable)
+{
+	lock_guard<recursive_mutex> lock(m_transport->GetMutex());
+    if (enable)
+        m_transport->SendCommandQueued("OUT1");
+    else
+        m_transport->SendCommandQueued("OUT0");
+}
---
+++ scopehal/GWInstekGPDX303SPowerSupply.h
@@ -0,0 +1,93 @@
+/***********************************************************************************************************************
+*                                                                                                                      *
+* libscopehal v0.1                                                                                                     *
+*                                                                                                                      *
+* Copyright (c) 2012-2022 Andrew D. Zonenberg and contributors                                                         *
+* All rights reserved.                                                                                                 *
+*                                                                                                                      *
+* Redistribution and use in source and binary forms, with or without modification, are permitted provided that the     *
+* following conditions are met:                                                                                        *
+*                                                                                                                      *
+*    * Redistributions of source code must retain the above copyright notice, this list of conditions, and the         *
+*      following disclaimer.                                                                                           *
+*                                                                                                                      *
+*    * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the       *
+*      following disclaimer in the documentation and/or other materials provided with the distribution.                *
+*                                                                                                                      *
+*    * Neither the name of the author nor the names of any contributors may be used to endorse or promote products     *
+*      derived from this software without specific prior written permission.                                           *
+*                                                                                                                      *
+* THIS SOFTWARE IS PROVIDED BY THE AUTHORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED   *
+* TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL *
+* THE AUTHORS BE HELD LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES        *
+* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR       *
+* BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT *
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE       *
+* POSSIBILITY OF SUCH DAMAGE.                                                                                          *
+*                                                                                                                      *
+***********************************************************************************************************************/
+
+#ifndef GWInstekGPDX303SPowerSupply_h
+#define GWInstekGPDX303SPowerSupply_h
+
+#include "SCPIDevice.h"
+#include "SCPIPowerSupply.h"
+#include "SCPITransport.h"
+
+#include <string>
+
+/**
+	@brief A GW Instek GPD-(X)303S power supply
+ */
+class GWInstekGPDX303SPowerSupply
+	: public virtual SCPIPowerSupply
+	, public virtual SCPIDevice
+{
+    GWInstekGPDX303SPowerSupply(SCPITransport* transport);
+	virtual ~GWInstekGPDX303SPowerSupply();
+
+	//Device information
+	virtual std::string GetName();
+	virtual std::string GetVendor();
+	virtual std::string GetSerial();
+
+	virtual unsigned int GetInstrumentTypes();
+
+	//Channel info
+	virtual int GetPowerChannelCount();
+	virtual std::string GetPowerChannelName(int chan);
+
+	//Read sensors
+	virtual double GetPowerVoltageActual(int chan);				//actual voltage after current limiting
+	virtual double GetPowerVoltageNominal(int chan);			//set point
+	virtual double GetPowerCurrentActual(int chan);				//actual current drawn by the load
+	virtual double GetPowerCurrentNominal(int chan);			//current limit
+	virtual bool GetPowerChannelActive(int chan);
+
+	//Configuration
+	virtual bool GetPowerOvercurrentShutdownEnabled(int chan);	//shut channel off entirely on overload,
+																//rather than current limiting
+	virtual void SetPowerOvercurrentShutdownEnabled(int chan, bool enable);
+	virtual bool GetPowerOvercurrentShutdownTripped(int chan);
+	virtual void SetPowerVoltage(int chan, double volts);
+	virtual void SetPowerCurrent(int chan, double amps);
+	virtual void SetPowerChannelActive(int chan, bool on);
+	virtual bool IsPowerConstantCurrent(int chan);
+
+	virtual bool GetMasterPowerEnable();
+	virtual void SetMasterPowerEnable(bool enable);
+
+	virtual bool IsSoftStartEnabled(int chan);
+	virtual void SetSoftStartEnabled(int chan, bool enable);
+
+protected:
+	uint8_t GetStatusRegister();
+
+	int m_channelCount;
+
+public:
+	static std::string GetDriverNameInternal();
+	POWER_INITPROC(GWInstekGPDX303SPowerSupply)
+};
+
+#endif
