b2955320729a95497210a20574e32834d0887e57
Andrew D. Zonenberg <azonenberg@drawersteak.com>
2024-09-19 21:24:29 -0700

    Started work on improving Doxygen coverage

--- scopehal/CopperMountainVNA.h
+++ scopehal/CopperMountainVNA.h
@@ -1,8 +1,8 @@
 /***********************************************************************************************************************
 *                                                                                                                      *
-* libscopehal v0.1                                                                                                     *
+* libscopehal                                                                                                          *
 *                                                                                                                      *
-* Copyright (c) 2012-2023 Andrew D. Zonenberg and contributors                                                         *
+* Copyright (c) 2012-2024 Andrew D. Zonenberg and contributors                                                         *
 * All rights reserved.                                                                                                 *
 *                                                                                                                      *
 * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the     *
@@ -27,11 +27,18 @@
 *                                                                                                                      *
 ***********************************************************************************************************************/
 
+/**
+	@file
+	@brief Declaration of CopperMountainVNA
+	@ingroup vnadrivers
+ */
+
 #ifndef CopperMountainVNA_h
 #define CopperMountainVNA_h
 
 /**
 	@brief Driver for Copper Mountain VNAs
+	@ingroup vnadrivers
 
 	So far, only tested on a S5180B
  */
