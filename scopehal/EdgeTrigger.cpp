9fe642d900957beea8a922aa8a7d31a6ad0c81d9
Andrew Zonenberg <azonenberg@drawersteak.com>
2024-10-18 23:01:24 -0700

    More doxygen comments

--- scopehal/EdgeTrigger.cpp
+++ scopehal/EdgeTrigger.cpp
@@ -1,8 +1,8 @@
 /***********************************************************************************************************************
 *                                                                                                                      *
-* libscopehal v0.1                                                                                                     *
+* libscopehal                                                                                                          *
 *                                                                                                                      *
-* Copyright (c) 2012-2023 Andrew D. Zonenberg and contributors                                                         *
+* Copyright (c) 2012-2024 Andrew D. Zonenberg and contributors                                                         *
 * All rights reserved.                                                                                                 *
 *                                                                                                                      *
 * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the     *
@@ -26,6 +26,13 @@
 * POSSIBILITY OF SUCH DAMAGE.                                                                                          *
 *                                                                                                                      *
 ***********************************************************************************************************************/
+
+/**
+	@file
+	@author Andrew D. Zonenberg
+	@brief Implementation of EdgeTrigger
+	@ingroup triggers
+ */
 
 #include "scopehal.h"
 #include "EdgeTrigger.h"
