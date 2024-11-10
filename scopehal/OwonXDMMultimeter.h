2879985bd50f6889e6ffd799913a86dd0b32235f
Frederic Borry <fborry@lyra-network.com>
2024-11-10 23:49:38 +0100

    Driver for Owon XDM meters.

---
+++ scopehal/OwonXDMMultimeter.h
@@ -0,0 +1,107 @@
+/***********************************************************************************************************************
+*                                                                                                                      *
+* libscopehal                                                                                                          *
+*                                                                                                                      *
+* Copyright (c) 2012-2024 Andrew D. Zonenberg and contributors                                                         *
+* All rights reserved.                                                                                                 *
+*                                                                                                                      *
+* Redistribution and use in source and binary forms, with or without modification, are permitted provided that the     *
+* following conditions are met:                                                                                        *
+*                                                                                                                      *
+*    * Redistributions of source code must retain the above copyright notice, this list of conditions, and the         *
+*      following disclaimer.                                                                                           *
+*                                                                                                                      *
+*    * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the       *
+*      following disclaimer in the documentation and/or other materials provided with the distribution.                *
+*                                                                                                                      *
+*    * Neither the name of the author nor the names of any contributors may be used to endorse or promote products     *
+*      derived from this software without specific prior written permission.                                           *
+*                                                                                                                      *
+* THIS SOFTWARE IS PROVIDED BY THE AUTHORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED   *
+* TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL *
+* THE AUTHORS BE HELD LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES        *
+* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR       *
+* BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT *
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE       *
+* POSSIBILITY OF SUCH DAMAGE.                                                                                          *
+*                                                                                                                      *
+***********************************************************************************************************************/
+
+/**
+	@file
+	@author Frederic BORRY
+	@brief Owon XDM Multimeter series driver
+ */
+#ifndef OwonXDMMultimeter_h
+#define OwonXDMMultimeter_h
+
+/**
+	@brief Driver for Owon XDM multimeter
+ */
+class OwonXDMMultimeter
+	: public virtual SCPIMultimeter
+{
+public:
+	OwonXDMMultimeter(SCPITransport* transport);
+	virtual ~OwonXDMMultimeter();
+
+	virtual unsigned int GetInstrumentTypes() const override;
+	virtual uint32_t GetInstrumentTypesForChannel(size_t i) const override;
+
+	virtual unsigned int GetMeasurementTypes() override;
+	virtual unsigned int GetSecondaryMeasurementTypes() override;
+
+	//Channel info
+	virtual int GetCurrentMeterChannel() override;
+	virtual void SetCurrentMeterChannel(int chan) override;
+
+	//Meter operating mode
+	virtual MeasurementTypes GetMeterMode() override;
+	virtual MeasurementTypes GetSecondaryMeterMode() override;
+	virtual void SetMeterMode(MeasurementTypes type) override;
+	virtual void SetSecondaryMeterMode(MeasurementTypes type) override;
+
+	//Control
+	virtual void SetMeterAutoRange(bool enable) override;
+	virtual bool GetMeterAutoRange() override;
+	virtual void StartMeter() override;
+	virtual void StopMeter() override;
+
+	virtual int GetMeterDigits() override;
+
+	//Get readings
+	virtual double GetMeterValue() override;
+	virtual double GetSecondaryMeterValue() override;
+
+protected:
+	bool m_modeValid;
+	bool m_secmodeValid;
+	MeasurementTypes m_mode;
+	MeasurementTypes m_secmode;
+
+	/**
+	 * @brief Remove CR from the provided line
+	 * 
+	 * @param toClean the line to remove the CR from (in/out)
+	 */
+	static void RemoveCR(std::string &toClean)
+	{
+		toClean.erase( std::remove(toClean.begin(), toClean.end(), '\r'), toClean.end() );
+	}
+
+	/**
+	 * @brief Remove quotes from the provided line
+	 * 
+	 * @param toClean the line to remove the quotes from (in/out)
+	 */
+	static void RemoveQuotes(std::string &toClean)
+	{
+		toClean.erase( std::remove(toClean.begin(), toClean.end(), '"'), toClean.end() );
+	}
+
+public:
+	static std::string GetDriverNameInternal();
+	METER_INITPROC(OwonXDMMultimeter)
+};
+
+#endif
