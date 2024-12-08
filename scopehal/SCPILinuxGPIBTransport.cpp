a11d51e2e186689279adf1140d9b2d8891913eb2
Andrew D. Zonenberg <azonenberg@drawersteak.com>
2024-12-08 11:19:50 -0800

    SCPILinuxGPIBTransport: fixed prototype missed during refactoring when we added progress callback. Fixes #945.

--- scopehal/SCPILinuxGPIBTransport.cpp
+++ scopehal/SCPILinuxGPIBTransport.cpp
@@ -1,8 +1,8 @@
 /***********************************************************************************************************************
 *                                                                                                                      *
-* libscopehal v0.1                                                                                                     *
+* libscopehal                                                                                                          *
 *                                                                                                                      *
-* Copyright (c) 2012-2022 Andrew D. Zonenberg and contributors                                                         *
+* Copyright (c) 2012-2024 Andrew D. Zonenberg and contributors                                                         *
 * All rights reserved.                                                                                                 *
 *                                                                                                                      *
 * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the     *
@@ -118,7 +118,7 @@ bool SCPILinuxGPIBTransport::SendCommand(const string& cmd)
 	return (ibcnt == (int)tempbuf.length());
 }
 
-string SCPILinuxGPIBTransport::ReadReply(bool endOnSemicolon)
+string SCPILinuxGPIBTransport::ReadReply(bool endOnSemicolon, [[maybe_unused]] function<void(float)> progress)
 {
 	string ret;
 	if (!IsConnected())
--- scopehal/SCPILinuxGPIBTransport.h
+++ scopehal/SCPILinuxGPIBTransport.h
@@ -1,8 +1,8 @@
 /***********************************************************************************************************************
 *                                                                                                                      *
-* libscopehal v0.1                                                                                                     *
+* libscopehal                                                                                                          *
 *                                                                                                                      *
-* Copyright (c) 2012-2022 Andrew D. Zonenberg and contributors                                                         *
+* Copyright (c) 2012-2024 Andrew D. Zonenberg and contributors                                                         *
 * All rights reserved.                                                                                                 *
 *                                                                                                                      *
 * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the     *
@@ -55,7 +55,7 @@ public:
 
 	void FlushRXBuffer(void) override;
 	bool SendCommand(const std::string& cmd) override;
-	std::string ReadReply(bool endOnSemicolon = true) override;
+	std::string ReadReply(bool endOnSemicolon = true, std::function<void(float)> progress = nullptr) override;
 	size_t ReadRawData(size_t len, unsigned char* buf, std::function<void(float)> progress = nullptr) override;
 	void SendRawData(size_t len, const unsigned char* buf) override;
 
