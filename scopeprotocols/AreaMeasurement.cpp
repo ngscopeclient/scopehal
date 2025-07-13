d7942ec74bb5bea18c34602e3eb4887694b51e06
Andrew D. Zonenberg <azonenberg@drawersteak.com>
2025-07-13 14:10:49 -0700

    AreaMeasurement: refactoring to use KahanSummation class, added scalar output. Fixes #458.

--- scopeprotocols/AreaMeasurement.cpp
+++ scopeprotocols/AreaMeasurement.cpp
@@ -2,7 +2,7 @@
 *                                                                                                                      *
 * libscopeprotocols                                                                                                    *
 *                                                                                                                      *
-* Copyright (c) 2012-2023 Andrew D. Zonenberg and contributors                                                         *
+* Copyright (c) 2012-2025 Andrew D. Zonenberg and contributors                                                         *
 * All rights reserved.                                                                                                 *
 *                                                                                                                      *
 * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the     *
@@ -28,6 +28,7 @@
 ***********************************************************************************************************************/
 
 #include "../scopehal/scopehal.h"
+#include "../scopehal/KahanSummation.h"
 #include "AreaMeasurement.h"
 
 using namespace std;
@@ -39,6 +40,7 @@ AreaMeasurement::AreaMeasurement(const string& color)
 	: Filter(color, CAT_MEASUREMENT)
 {
 	AddStream(Unit(Unit::UNIT_VOLT_SEC), "data", Stream::STREAM_TYPE_ANALOG);
+	AddStream(Unit(Unit::UNIT_VOLTS), "final", Stream::STREAM_TYPE_ANALOG_SCALAR);
 
 	//Set up channels
 	CreateInput("din");
@@ -59,7 +61,7 @@ AreaMeasurement::AreaMeasurement(const string& color)
 
 bool AreaMeasurement::ValidateChannel(size_t i, StreamDescriptor stream)
 {
-	if(stream.m_channel == NULL)
+	if(stream.m_channel == nullptr)
 		return false;
 
 	if(i > 0)
@@ -87,7 +89,8 @@ void AreaMeasurement::Refresh()
 	//Make sure we've got valid inputs
 	if(!VerifyAllInputsOK())
 	{
-		SetData(NULL, 0);
+		SetData(nullptr, 0);
+		m_streams[1].m_value = std::numeric_limits<float>::quiet_NaN();
 		return;
 	}
 
@@ -97,8 +100,6 @@ void AreaMeasurement::Refresh()
 	auto uadin = dynamic_cast<UniformAnalogWaveform*>(din);
 	auto sadin = dynamic_cast<SparseAnalogWaveform*>(din);
 	auto length = din->size();
-	float area = 0;
-	float c = 0;
 
 	MeasurementType measurement_type = (MeasurementType)m_parameters[m_measurement_typename].GetIntVal();
 	AreaType area_type = (AreaType)m_parameters[m_area_typename].GetIntVal();
@@ -107,21 +108,19 @@ void AreaMeasurement::Refresh()
 	{
 		if(uadin)
 		{
-			//Create the output as a uniform waveform with single sample
+			//Create the output as a uniform waveform
 			auto cap = SetupEmptyUniformAnalogOutputWaveform(din, 0, true);
 			cap->PrepareForCpuAccess();
 
-			//Perform summation using Kahan Summation Algorithm. Use volatile to prevent optimizations
+			float samp = 0;
+			KahanSummation sum;
 			for (size_t i = 0; i < length; i++)
 			{
-				float y = (((area_type == TRUE_AREA) ? uadin->m_samples[i] : fabs(uadin->m_samples[i])) * din->m_timescale) - c;
-				volatile float t = area + y;
-				volatile float z = t - area;
-				c = z - y;
-				area = t;
-
-				cap->m_samples.push_back(area / FS_PER_SECOND);
+				sum += (((area_type == TRUE_AREA) ? uadin->m_samples[i] : fabs(uadin->m_samples[i])) * din->m_timescale);
+				samp = sum.GetSum() / FS_PER_SECOND;
+				cap->m_samples.push_back(samp);
 			}
+			m_streams[1].m_value = samp;
 
 			SetData(cap, 0);
 			cap->MarkModifiedFromCpu();
@@ -132,21 +131,20 @@ void AreaMeasurement::Refresh()
 			auto cap = SetupEmptySparseAnalogOutputWaveform(din, 0, true);
 			cap->PrepareForCpuAccess();
 
-			//Perform summation using Kahan Summation Algorithm. Use volatile to prevent optimizations
+			float samp = 0;
+			KahanSummation sum;
 			for (size_t i = 0; i < length; i++)
 			{
-				float y = (((area_type == TRUE_AREA) ? sadin->m_samples[i] : fabs(sadin->m_samples[i])) * sadin->m_durations[i]
-																										* din->m_timescale) - c;
-				volatile float t = area + y;
-				volatile float z = t - area;
-				c = z - y;
-				area = t;
+				sum += (((area_type == TRUE_AREA) ? sadin->m_samples[i] : fabs(sadin->m_samples[i])) *
+					sadin->m_durations[i] * din->m_timescale);
 
 				//Push values to the waveform
+				samp = sum.GetSum() / FS_PER_SECOND;
 				cap->m_offsets.push_back(sadin->m_offsets[i]);
 				cap->m_durations.push_back(sadin->m_durations[i]);
-				cap->m_samples.push_back(area / FS_PER_SECOND);
+				cap->m_samples.push_back(samp);
 			}
+			m_streams[1].m_value = samp;
 
 			SetData(cap, 0);
 			cap->MarkModifiedFromCpu();
@@ -166,7 +164,8 @@ void AreaMeasurement::Refresh()
 		//We need at least one full cycle of the waveform
 		if(edges.size() < 2)
 		{
-			SetData(NULL, 0);
+			SetData(nullptr, 0);
+			m_streams[1].m_value = std::numeric_limits<float>::quiet_NaN();
 			return;
 		}
 
@@ -184,28 +183,18 @@ void AreaMeasurement::Refresh()
 			int64_t end = edges[i + 2] / din->m_timescale;
 			int64_t j = 0;
 
+			KahanSummation sum;
 			if(uadin)
 			{
-				//Perform summation using Kahan Summation Algorithm. Use volatile to prevent optimizations
 				for(j = start; (j <= end) && (j < (int64_t)length); j++)
-				{
-					float y = ((area_type == TRUE_AREA) ? uadin->m_samples[j] : fabs(uadin->m_samples[j])) - c;
-					volatile float t = area + y;
-					volatile float z = t - area;
-					c = z - y;
-					area = t;
-				}
+					sum += ((area_type == TRUE_AREA) ? uadin->m_samples[j] : fabs(uadin->m_samples[j]));
 			}
 			else if(sadin)
 			{
-				//Perform summation using Kahan Summation Algorithm. Use volatile to prevent optimizations
 				for(j = start; (j <= end) && (j < (int64_t)length); j++)
 				{
-					float y = ((((area_type == TRUE_AREA) ? sadin->m_samples[j] : fabs(sadin->m_samples[j])) * sadin->m_durations[j])) - c;
-					volatile float t = area + y;
-					volatile float z = t - area;
-					c = z - y;
-					area = t;
+					sum += ((((area_type == TRUE_AREA) ? sadin->m_samples[j] : fabs(sadin->m_samples[j]))
+						* sadin->m_durations[j]));
 				}
 			}
 
@@ -218,13 +207,12 @@ void AreaMeasurement::Refresh()
 				//Push values to the waveform
 				cap->m_offsets.push_back(start);
 				cap->m_durations.push_back(delta);
-				cap->m_samples.push_back((area * din->m_timescale) / FS_PER_SECOND);
+				cap->m_samples.push_back((sum.GetSum() * din->m_timescale) / FS_PER_SECOND);
 			}
-
-			area = 0;
 		}
 
 		SetData(cap, 0);
 		cap->MarkModifiedFromCpu();
+		m_streams[1].m_value = std::numeric_limits<float>::quiet_NaN();
 	}
 }
