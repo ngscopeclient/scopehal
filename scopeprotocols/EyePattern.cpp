eea7adafbe70e3c0322a2daea54e660129c66382
Andrew D. Zonenberg <azonenberg@drawersteak.com>
2026-01-01 22:44:27 -0800

    Replaced NULL with nullptr

--- scopeprotocols/EyePattern.cpp
+++ scopeprotocols/EyePattern.cpp
@@ -130,7 +130,7 @@ EyePattern::EyePattern(const string& color)
 
 bool EyePattern::ValidateChannel(size_t i, StreamDescriptor stream)
 {
-	if(stream.m_channel == NULL)
+	if(stream.m_channel == nullptr)
 		return false;
 
 	if( (i == 0) && (stream.GetType() == Stream::STREAM_TYPE_ANALOG) )
