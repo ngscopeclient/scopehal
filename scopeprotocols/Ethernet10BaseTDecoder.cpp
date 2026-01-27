d236c77ffcd5d57c10d106c7774e079c1fdd3219
Andrew D. Zonenberg <azonenberg@drawersteak.com>
2026-01-26 22:09:57 -0800

    Ethernet10BaseTDecoder: refactoring and error reporting

--- scopeprotocols/Ethernet10BaseTDecoder.cpp
+++ scopeprotocols/Ethernet10BaseTDecoder.cpp
@@ -2,7 +2,7 @@
 *                                                                                                                      *
 * libscopeprotocols                                                                                                    *
 *                                                                                                                      *
-* Copyright (c) 2012-2022 Andrew D. Zonenberg and contributors                                                         *
+* Copyright (c) 2012-2026 Andrew D. Zonenberg and contributors                                                         *
 * All rights reserved.                                                                                                 *
 *                                                                                                                      *
 * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the     *
@@ -53,17 +53,35 @@ string Ethernet10BaseTDecoder::GetProtocolName()
 	return "Ethernet - 10baseT";
 }
 
+Filter::DataLocation Ethernet10BaseTDecoder::GetInputLocation()
+{
+	//We explicitly manage our input memory and don't care where it is when Refresh() is called
+	return LOC_DONTCARE;
+}
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Actual decoder logic
 
-void Ethernet10BaseTDecoder::Refresh()
+void Ethernet10BaseTDecoder::Refresh(
+	[[maybe_unused]] vk::raii::CommandBuffer& cmdBuf,
+	[[maybe_unused]] shared_ptr<QueueHandle> queue)
 {
+	#ifdef HAVE_NVTX
+		nvtx3::scoped_range nrange("Ethernet10BaseTDecoder::Refresh");
+	#endif
+
 	ClearPackets();
 
 	//Get the input data
+	ClearErrors();
 	if(!VerifyAllInputsOKAndUniformAnalog())
 	{
-		SetData(NULL, 0);
+		if(!GetInput(0))
+			AddErrorMessage("Missing inputs", "No signal input connected");
+		else if(!GetInputWaveform(0))
+			AddErrorMessage("Missing inputs", "No waveform available at input");
+
+		SetData(nullptr, 0);
 		return;
 	}
 
@@ -72,10 +90,7 @@ void Ethernet10BaseTDecoder::Refresh()
 	din->PrepareForCpuAccess();
 
 	//Copy our time scales from the input
-	auto cap = new EthernetWaveform;
-	cap->m_timescale = din->m_timescale;
-	cap->m_startTimestamp = din->m_startTimestamp;
-	cap->m_startFemtoseconds = din->m_startFemtoseconds;
+	auto cap = SetupEmptyWaveform<EthernetWaveform>(din, 0);
 	cap->PrepareForCpuAccess();
 
 	const int64_t ui_width 		= 100000 * 1000;
@@ -225,8 +240,6 @@ void Ethernet10BaseTDecoder::Refresh()
 		//Crunch the Manchester-coded data
 		BytesToFrames(bytes, starts, ends, cap);
 	}
-
-	SetData(cap, 0);
 
 	cap->MarkModifiedFromCpu();
 }
