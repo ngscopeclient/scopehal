0ab323232c46605b000c30e0319e3bcdb65df77a
Andrew D. Zonenberg <azonenberg@drawersteak.com>
2026-02-06 23:04:45 -0800

    J1939SourceMatch: Added missing ClearErrors call

--- scopeprotocols/J1939SourceMatchFilter.cpp
+++ scopeprotocols/J1939SourceMatchFilter.cpp
@@ -103,6 +103,7 @@ void J1939SourceMatchFilter::Refresh(
 	ClearPackets();
 
 	//Make sure we've got valid inputs
+	ClearErrors();
 	auto din = dynamic_cast<J1939PDUWaveform*>(GetInputWaveform(0));
 	if(!din)
 	{
