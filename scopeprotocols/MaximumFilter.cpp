ef4efd1da9b37961172e84adecdfaf6d67a38d04
Andrew D. Zonenberg <azonenberg@drawersteak.com>
2026-02-11 23:53:31 -0800

    MaximumFilter: switched to GPU implementation

--- scopeprotocols/MaximumFilter.cpp
+++ scopeprotocols/MaximumFilter.cpp
@@ -2,7 +2,7 @@
 *                                                                                                                      *
 * libscopeprotocols                                                                                                    *
 *                                                                                                                      *
-* Copyright (c) 2012-2024 Andrew D. Zonenberg and contributors                                                         *
+* Copyright (c) 2012-2026 Andrew D. Zonenberg and contributors                                                         *
 * All rights reserved.                                                                                                 *
 *                                                                                                                      *
 * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the     *
@@ -34,6 +34,7 @@ using namespace std;
 
 MaximumFilter::MaximumFilter(const string& color)
 	: Filter(color, CAT_MATH)
+	, m_computePipeline("shaders/MinMax.spv", 3, sizeof(uint32_t))
 {
 	AddStream(Unit(Unit::UNIT_VOLTS), "latest", Stream::STREAM_TYPE_ANALOG_SCALAR);
 	AddStream(Unit(Unit::UNIT_VOLTS), "cumulative", Stream::STREAM_TYPE_ANALOG_SCALAR);
@@ -52,11 +53,19 @@ MaximumFilter::~MaximumFilter()
 {
 }
 
-void MaximumFilter::Refresh(vk::raii::CommandBuffer& /*cmdBuf*/, shared_ptr<QueueHandle> /*queue*/)
+void MaximumFilter::Refresh(vk::raii::CommandBuffer& cmdBuf, shared_ptr<QueueHandle> queue)
 {
+	#ifdef HAVE_NVTX
+		nvtx3::scoped_range nrange("MaximumFilter::Refresh");
+	#endif
+
+	ClearErrors();
 	auto din = GetInput(0);
 	if(!din)
+	{
+		AddErrorMessage("Missing inputs", "No input connected");
 		return;
+	}
 
 	//Copy units to output streams
 	m_streams[0].m_yAxisUnit = din.GetYAxisUnits();
@@ -77,24 +86,25 @@ void MaximumFilter::Refresh(vk::raii::CommandBuffer& /*cmdBuf*/, shared_ptr<Queu
 	else
 	{
 		auto data = din.GetData();
-		auto udata = dynamic_cast<UniformAnalogWaveform*>(data);
-		auto sdata = dynamic_cast<SparseAnalogWaveform*>(data);
-		float total = -FLT_MAX;
 		size_t len = data->size();
 
+		auto udata = dynamic_cast<UniformAnalogWaveform*>(data);
+		auto sdata = dynamic_cast<SparseAnalogWaveform*>(data);
+
+		float vmin;
+		float vmax;
 		if(udata)
-		{
-			for(auto sample : udata->m_samples)
-				total = max(total, sample);
-		}
+			Filter::GetMinMaxVoltage(cmdBuf, queue, m_computePipeline, m_scratchMin, m_scratchMax, udata, vmin, vmax);
 		else if(sdata)
+			Filter::GetMinMaxVoltage(cmdBuf, queue, m_computePipeline, m_scratchMin, m_scratchMax, sdata, vmin, vmax);
+		else
 		{
-			for(auto sample : sdata->m_samples)
-				total = max(total, sample);
+			AddErrorMessage("Invalid inputs", "Expected sparse or uniform analog waveform");
+			return;
 		}
 
-		m_streams[0].m_value = total;
-		m_streams[1].m_value = max((double)total, m_streams[1].m_value);
+		m_streams[0].m_value = vmax;
+		m_streams[1].m_value = max((double)vmax, m_streams[1].m_value);
 		m_streams[2].m_value += len;
 		m_streams[3].m_value ++;
 	}
