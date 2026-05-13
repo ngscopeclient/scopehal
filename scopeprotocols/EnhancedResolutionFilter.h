21721de76dd1944d8ebcaf19ea7d99f2f104fcb1
Andrew D. Zonenberg <azonenberg@drawersteak.com>
2026-05-13 07:39:04 -0700

    Added false positive static analysis suppression

--- scopeprotocols/EnhancedResolutionFilter.h
+++ scopeprotocols/EnhancedResolutionFilter.h
@@ -2,7 +2,7 @@
 *                                                                                                                      *
 * libscopeprotocols                                                                                                    *
 *                                                                                                                      *
-* Copyright (c) 2012-2022 Andrew D. Zonenberg and contributors                                                         *
+* Copyright (c) 2012-2026 Andrew D. Zonenberg and contributors                                                         *
 * All rights reserved.                                                                                                 *
 *                                                                                                                      *
 * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the     *
@@ -45,6 +45,8 @@ public:
 	virtual void Refresh(vk::raii::CommandBuffer& cmdBuf, std::shared_ptr<QueueHandle> queue) override;
 	virtual void SetDefaultName() override;
 
+	//This is intentionally not virtual since it's a static method used by enumeration
+	//cppcheck-suppress duplInheritedMember
 	static std::string GetProtocolName();
 
 	PROTOCOL_DECODER_INITPROC(EnhancedResolutionFilter)
