1471568544b1e62ea9c5446541c403c34cdfd0a9
Andrew D. Zonenberg <azonenberg@drawersteak.com>
2026-05-26 14:22:31 -0700

    NoiseFilter: removed buggy AVX impl in preparation for GPU acceleration

--- scopeprotocols/NoiseFilter.cpp
+++ scopeprotocols/NoiseFilter.cpp
@@ -2,7 +2,7 @@
 *                                                                                                                      *
 * libscopeprotocols                                                                                                    *
 *                                                                                                                      *
-* Copyright (c) 2012-2022 Andrew D. Zonenberg and contributors                                                         *
+* Copyright (c) 2012-2026 Andrew D. Zonenberg and contributors                                                         *
 * All rights reserved.                                                                                                 *
 *                                                                                                                      *
 * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the     *
@@ -94,12 +94,7 @@ void NoiseFilter::Refresh()
 	cap->Resize(len);
 	cap->PrepareForCpuAccess();
 
-	#ifdef __x86_64__
-	if(g_hasAvx2)
-		CopyWithAwgnAVX2((float*)&cap->m_samples[0], (float*)&din->m_samples[0], len, stdev);
-	else
-	#endif
-		CopyWithAwgnNative((float*)&cap->m_samples[0], (float*)&din->m_samples[0], len, stdev);
+	CopyWithAwgnNative((float*)&cap->m_samples[0], (float*)&din->m_samples[0], len, stdev);
 
 	cap->MarkModifiedFromCpu();
 }
@@ -116,83 +111,3 @@ void NoiseFilter::CopyWithAwgnNative(float* dest, float* src, size_t len, float 
 		dest[i] = src[i] + noise(rng);
 #pragma GCC diagnostic pop
 }
-
-#ifdef __x86_64__
-__attribute__((target("avx2")))
-void NoiseFilter::CopyWithAwgnAVX2(float* dest, float* src, size_t len, float sigma)
-{
-	size_t end = len - (len % 16);
-
-	//Box-Muller setup
-	__m256 vsigma		= _mm256_set1_ps(sigma);
-	__m256 vmtwo		= _mm256_set1_ps(-2.0f);
-	__m256 vtpi			= _mm256_set1_ps(M_PI * 2);
-	__m256 norm1;
-	__m256 norm2;
-
-	//Random number generator (xorshift32 LFSR)
-	__m256i rng_state1 = _mm256_set_epi32(
-		m_twister(), m_twister(), m_twister(), m_twister(), m_twister(), m_twister(), m_twister(), m_twister());
-	__m256i rng_state2 = _mm256_set_epi32(
-		m_twister(), m_twister(), m_twister(), m_twister(), m_twister(), m_twister(), m_twister(), m_twister());
-	__m256 rng_scale	= _mm256_set1_ps(1.0f / static_cast<float>(0xffffffff));
-
-	//Create normally distributed output using Box-Muller
-	for(size_t i=0; i<end; i += 16)
-	{
-		//Load input samples
-		__m256 samples1		= _mm256_load_ps(src + i);
-		__m256 samples2		= _mm256_load_ps(src + i + 8);
-
-		//Generate two sets of eight random int32 values
-		__m256i tmp1		= _mm256_slli_epi32(rng_state1, 13);
-		__m256i tmp2		= _mm256_slli_epi32(rng_state2, 13);
-		rng_state1			= _mm256_xor_si256(rng_state1, tmp1);
-		rng_state2			= _mm256_xor_si256(rng_state2, tmp2);
-		tmp1				= _mm256_srli_epi32(rng_state1, 17);
-		tmp2				= _mm256_srli_epi32(rng_state2, 17);
-		rng_state1			= _mm256_xor_si256(rng_state1, tmp1);
-		rng_state2			= _mm256_xor_si256(rng_state2, tmp2);
-		tmp1				= _mm256_slli_epi32(rng_state1, 5);
-		tmp2				= _mm256_slli_epi32(rng_state2, 5);
-		rng_state1			= _mm256_xor_si256(rng_state1, tmp1);
-		rng_state2			= _mm256_xor_si256(rng_state2, tmp2);
-
-		//Convert the random values to floating point in the range (0, 1)
-		tmp1				= _mm256_abs_epi32(rng_state1);
-		tmp2				= _mm256_abs_epi32(rng_state2);
-		__m256 random1		= _mm256_cvtepi32_ps(tmp1);
-		__m256 random2		= _mm256_cvtepi32_ps(tmp2);
-		random1				= _mm256_mul_ps(random1, rng_scale);
-		random2				= _mm256_mul_ps(random2, rng_scale);
-
-		//Apply Box-Muller transformation
-		__m256 mag			= _mm256_log_ps(random1);
-		mag					= _mm256_mul_ps(mag, vmtwo);
-		mag					= _mm256_sqrt_ps(mag);
-		mag					= _mm256_mul_ps(mag, vsigma);
-		__m256 rtpi			= _mm256_mul_ps(random2, vtpi);
-		_mm256_sincos_ps(rtpi, &norm1, &norm2);
-		norm1				= _mm256_mul_ps(mag, norm1);
-		norm2				= _mm256_mul_ps(mag, norm2);
-
-		//Add the noise
-		__m256 out1			= _mm256_add_ps(samples1, norm1);
-		__m256 out2			= _mm256_add_ps(samples2, norm2);
-
-		//Write output
-		_mm256_store_ps(dest + i, out1);
-		_mm256_store_ps(dest + i + 8, out2);
-	}
-
-	//Process the last few samples
-	//gcc 8.x / 9.x have false positive here (https://gcc.gnu.org/bugzilla/show_bug.cgi?id=99536)
-	minstd_rand rng(m_twister());
-	normal_distribution<> noise(0, sigma);
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wmaybe-uninitialized"
-	for(size_t i=end; i<len; i++)
-		dest[i] = src[i] + noise(rng);
-#pragma GCC diagnostic pop
-}
-#endif /* __x86_64__ */
--- scopeprotocols/NoiseFilter.h
+++ scopeprotocols/NoiseFilter.h
@@ -2,7 +2,7 @@
 *                                                                                                                      *
 * libscopeprotocols                                                                                                    *
 *                                                                                                                      *
-* Copyright (c) 2012-2022 Andrew D. Zonenberg and contributors                                                         *
+* Copyright (c) 2012-2026 Andrew D. Zonenberg and contributors                                                         *
 * All rights reserved.                                                                                                 *
 *                                                                                                                      *
 * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the     *
@@ -51,9 +51,6 @@ public:
 	PROTOCOL_DECODER_INITPROC(NoiseFilter)
 
 protected:
-#ifdef __x86_64__
-	void CopyWithAwgnAVX2(float* dest, float* src, size_t len, float sigma);
-#endif
 	void CopyWithAwgnNative(float* dest, float* src, size_t len, float sigma);
 
 	std::string m_stdevname;
