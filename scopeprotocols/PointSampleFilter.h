0dd0b084dd5ce19713bc4cba52fa57e9b0b103d2
Andrew D. Zonenberg <azonenberg@drawersteak.com>
2026-06-01 03:37:23 -0700

    PointSampleFilter: refactoring, allow taking input from anywhere

--- scopeprotocols/PointSampleFilter.h
+++ scopeprotocols/PointSampleFilter.h
@@ -2,7 +2,7 @@
 *                                                                                                                      *
 * libscopeprotocols                                                                                                    *
 *                                                                                                                      *
-* Copyright (c) 2012-2025 Andrew D. Zonenberg and contributors                                                         *
+* Copyright (c) 2012-2026 Andrew D. Zonenberg and contributors                                                         *
 * All rights reserved.                                                                                                 *
 *                                                                                                                      *
 * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the     *
@@ -51,7 +51,7 @@ public:
 	PROTOCOL_DECODER_INITPROC(PointSampleFilter)
 
 protected:
-	std::string m_offname;
+	FilterParameter& m_off;
 };
 
 #endif
