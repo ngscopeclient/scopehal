a0cfc5b4a4062ad6fb88b8da2f16e9b3e27a893d
Andrew D. Zonenberg <azonenberg@drawersteak.com>
2026-06-02 12:09:42 -0700

    UartClockRecoveryFilter: refactoring

--- scopeprotocols/UartClockRecoveryFilter.h
+++ scopeprotocols/UartClockRecoveryFilter.h
@@ -2,7 +2,7 @@
 *                                                                                                                      *
 * libscopeprotocols                                                                                                    *
 *                                                                                                                      *
-* Copyright (c) 2012-2022 Andrew D. Zonenberg and contributors                                                         *
+* Copyright (c) 2012-2026 Andrew D. Zonenberg and contributors                                                         *
 * All rights reserved.                                                                                                 *
 *                                                                                                                      *
 * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the     *
@@ -40,7 +40,8 @@ class UartClockRecoveryFilter : public Filter
 public:
 	UartClockRecoveryFilter(const std::string& color);
 
-	virtual void Refresh() override;
+	virtual void Refresh(vk::raii::CommandBuffer& cmdBuf, std::shared_ptr<QueueHandle> queue) override;
+	virtual DataLocation GetInputLocation() override;
 
 	static std::string GetProtocolName();
 
@@ -49,8 +50,8 @@ public:
 	PROTOCOL_DECODER_INITPROC(UartClockRecoveryFilter)
 
 protected:
-	std::string m_baudname;
-	std::string m_threshname;
+	FilterParameter& m_baud;
+	FilterParameter& m_thresh;
 };
 
 #endif
