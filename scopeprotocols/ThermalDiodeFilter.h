a00ea677c8012bd7ed6e5d636691d43abe86142d
Andrew D. Zonenberg <azonenberg@drawersteak.com>
2026-06-02 10:10:52 -0700

    ThermalDiodeFilter: refactoring

--- scopeprotocols/ThermalDiodeFilter.h
+++ scopeprotocols/ThermalDiodeFilter.h
@@ -2,7 +2,7 @@
 *                                                                                                                      *
 * libscopeprotocols                                                                                                    *
 *                                                                                                                      *
-* Copyright (c) 2012-2023 Andrew D. Zonenberg and contributors                                                         *
+* Copyright (c) 2012-2026 Andrew D. Zonenberg and contributors                                                         *
 * All rights reserved.                                                                                                 *
 *                                                                                                                      *
 * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the     *
@@ -44,6 +44,7 @@ public:
 	~ThermalDiodeFilter();
 
 	virtual void Refresh(vk::raii::CommandBuffer& cmdBuf, std::shared_ptr<QueueHandle> queue) override;
+	virtual DataLocation GetInputLocation() override;
 
 	static std::string GetProtocolName();
 
@@ -58,7 +59,7 @@ protected:
 		DIODE_LTC3374A,
 	};
 
-	std::string m_diodeType;
+	FilterParameter& m_diodeType;
 };
 
 #endif
